94ec44e93193d1b4e1cba339babe61455107d7c5
Michael Fellinger <michael.fellinger@iohk.io>
2020-01-08 17:12:29 +0100

    improve all the things

--- src/nixy.cr
+++ src/nixy.cr
@@ -6,20 +6,18 @@ module Nixy
   VERSION = "0.1.0"
 
   PACKAGES_JSON = File.join ENV["XDG_CONFIG_HOME"], "/nixy/packages.json"
-  PACKAGES_NIX = File.join ENV["XDG_CONFIG_HOME"], "/nixy/packages.nix"
+  PACKAGES_NIX  = File.join ENV["XDG_CONFIG_HOME"], "/nixy/packages.nix"
+  SOURCES_JSON  = File.join ENV["XDG_CONFIG_HOME"], "/nixy/sources.json"
 
   PACKAGES_NIX_TEMPLATE = {{ read_file("#{__DIR__}/packages.nix") }}
 
   module RegistryHelper
     def profile_result(attr : String) : String | Nil
-      FileUtils.mkdir_p File.dirname(PACKAGES_NIX)
-      File.write(PACKAGES_NIX, PACKAGES_NIX_TEMPLATE)
-
       args = [PACKAGES_NIX, "--no-out-link",
               "--argstr", "name", profile_name,
               "--argstr", "packagesJSON", PACKAGES_JSON,
-              "-A", attr
-             ]
+              "-A", attr,
+      ]
 
       output = IO::Memory.new
       status = Process.run("nix-build", args, error: STDERR, output: output)
@@ -30,15 +28,7 @@ module Nixy
       "nixy-env"
     end
 
-    private def ensure_registry
-      FileUtils.mkdir_p File.dirname(PACKAGES_JSON)
-      return if File.file?(PACKAGES_JSON)
-      File.write(PACKAGES_JSON, Registry.new([] of String).to_pretty_json)
-    end
-
     def registry!(&block : Registry -> _)
-      ensure_registry
-
       old = File.read(PACKAGES_JSON)
       reg = Registry.from_json(old).tap(&block)
 
@@ -67,8 +57,6 @@ module Nixy
     end
 
     def registry(&block : Registry -> _)
-      ensure_registry
-
       File.open(PACKAGES_JSON) do |io|
         Registry.from_json(io).tap(&block)
       end
@@ -137,6 +125,122 @@ module Nixy
         end
       end
     end
+
+    class Init < Easy_CLI::Command
+      include Nixy::RegistryHelper
+
+      def initialize
+        name "init"
+        desc "Install files needed for using Nixy"
+      end
+
+      def call(data)
+        unless File.file?(PACKAGES_NIX)
+          FileUtils.mkdir_p File.dirname(PACKAGES_NIX)
+          File.write(PACKAGES_NIX, PACKAGES_NIX_TEMPLATE)
+        end
+
+        unless File.file?(PACKAGES_JSON)
+          FileUtils.mkdir_p File.dirname(PACKAGES_JSON)
+          File.write(PACKAGES_JSON, Registry.new([] of String).to_pretty_json)
+        end
+
+        unless File.file?(SOURCES_JSON)
+          FileUtils.mkdir_p File.dirname(SOURCES_JSON)
+          File.write(
+            SOURCES_JSON,
+            {
+              "soures" => {
+                "nixpkgs" => {
+                  "branch"       => "nixpkgs-unstable",
+                  "owner"        => "NixOS",
+                  "repo"         => "nixpkgs-channels",
+                  "rev"          => "cc6cf0a96a627e678ffc996a8f9d1416200d6c81",
+                  "sha256"       => "1srjikizp8ip4h42x7kr4qf00lxcp1l8zp6h0r1ddfdyw8gv9001",
+                  "type"         => "github",
+                  "url_template" => "https://github.com/<owner>/<repo>/archive/<rev>.tar.gz",
+                },
+              },
+            }.to_pretty_json
+          )
+        end
+      end
+    end
+
+    class Sources < Easy_CLI::Command
+      include Nixy::RegistryHelper
+
+      def initialize
+        name "sources"
+        desc "Manipulate the sources for your packages"
+
+        register Update.new
+        register List.new
+        register Add.new
+      end
+
+      def call(data)
+        raise "Not implemented"
+      end
+
+      class List < Easy_CLI::Command
+        def initialize
+          name "list"
+          desc "List sources"
+        end
+
+        def call(data)
+          sources = Nixy::Sources.from_json(File.read(SOURCES_JSON))
+          sources.sources.each do |name, value|
+            pp! name
+            pp! value
+            pp! value.url
+          end
+        end
+      end
+
+      class Update < Easy_CLI::Command
+        def initialize
+          name "update"
+          desc "Update your sources"
+        end
+
+        def call(data)
+          sources = Nixy::Sources.from_json(File.read(SOURCES_JSON))
+          sources.sources.each do |name, value|
+            value.update
+          end
+
+          File.write(SOURCES_JSON, sources.to_pretty_json)
+        end
+      end
+
+      class Add < Easy_CLI::Command
+        def initialize
+          name "add"
+          desc "Add entry to your sources"
+          argument "url"
+        end
+
+        def call(data)
+          case url = data["url"]
+          when %r(([^/]+)/([^/]+))
+            sources = Nixy::Sources.from_json(File.read(SOURCES_JSON))
+            sources.sources[$2] = Nixy::Sources::Source.new(
+              owner: $1,
+              repo: $2,
+              type: "github",
+              branch: "master",
+              url_template: "https://github.com/<owner>/<repo>/archive/<rev>.tar.gz"
+            )
+            File.write(SOURCES_JSON, sources.to_pretty_json)
+          else
+            raise "This syntax is not supported yet, try 'owner/repo'"
+            pp! url
+          end
+        end
+      end
+    end
   end
 
   class Registry
@@ -150,12 +254,85 @@ module Nixy
     end
 
     def remove(package)
-      self.packages = (packages  - [package]).sort
+      self.packages = (packages - [package]).sort
     end
 
     def list
       packages.each do |package|
         puts package
+      end
+    end
+  end
+
+  class Sources
+    JSON.mapping(sources: Hash(String, Source))
+
+    class Source
+      JSON.mapping(
+        type: String,
+        url_template: String,
+        branch: String,
+        owner: String,
+        repo: String,
+        rev: String,
+        sha256: String,
+      )
+
+      def initialize(@owner, @repo, @type, @branch, @url_template)
+        @rev = update_rev
+        @sha256 = update_sha256
+      end
+
+      def url
+        url_template.gsub(/<([^>]+)>/) {
+          self[$1]
+        }
+      end
+
+      def update
+        update_rev
+        update_sha256
+      end
+
+      def update_rev
+        output = IO::Memory.new
+
+        case type
+        when "github"
+          Process.run("git", ["ls-remote", "https://github.com/#{owner}/#{repo}", branch], output: output)
+        end
+
+        hash = output.to_s.split("\t").first.strip
+        puts "Found rev: #{hash}"
+        @rev = hash
+      end
+
+      def update_sha256
+        output = IO::Memory.new
+
+        case type
+        when "github"
+          Process.run("nix-prefetch-url", ["--unpack", url], output: output)
+        end
+
+        hash = output.to_s.strip
+        puts "Found sha256: #{hash}"
+        @sha256 = hash
+      end
+
+      def [](key)
+        case key
+        when "rev"
+          rev
+        when "branch"
+          branch
+        when "owner"
+          owner
+        when "repo"
+          repo
+        else
+          raise "Invalid key used in url_template: '#{key}'"
+        end
       end
     end
   end
@@ -166,6 +343,8 @@ module Nixy
     cli.register UI::Add.new
     cli.register UI::Remove.new
     cli.register UI::List.new
+    cli.register UI::Init.new
+    cli.register UI::Sources.new
     cli.run(args)
   end
 end
--- src/packages.nix
+++ src/packages.nix
@@ -1,17 +1,38 @@
 # WARNING: This file is auto-generated, all modifications may disappear without notice.
-{ pkgs ? import ~/nixos-conf/nix { }, name ? "nixy-env"
-, packagesJSON ? ./packages.json }: rec {
+{ name ? "nixy-env", packagesJSON ? ./packages.json
+, sourcesJSON ? ./sources.json }: rec {
+  getName = x:
+    let parse = drv: (builtins.parseDrvName drv).name;
+    in if builtins.isString x then parse x else x.pname or (parse x.name);
+
+  fetchers = {
+    github = spec: builtins.fetchTarball { inherit (spec) sha256 url; };
+  };
+
+  mkSource = name: spec: fetchers."${spec.type}" (pp spec);
+  sources = builtins.mapAttrs mkSource
+    (builtins.fromJSON (builtins.readFile sourcesJSON)).sources;
+
+  pp = v: __trace (__toJSON v) v;
+
+  inherit (nixpkgs) buildEnv lib;
+  inherit (lib) splitString getAttrFromPath;
+
+  nixpkgs = import sources.nixpkgs {
+    overlays = import ./overlays.nix { inherit sources; };
+  };
+
   paths = map (name:
-    let path = pkgs.lib.splitString "." name;
-    in pkgs.lib.getAttrFromPath path pkgs)
+    let path = splitString "." name;
+    in getAttrFromPath path nixpkgs)
     (builtins.fromJSON (builtins.readFile packagesJSON)).packages;
 
   listing = map (pkg: {
-    name = pkgs.lib.getName pkg;
+    name = getName pkg;
     description = pkg.meta.description or null;
   }) paths;
 
-  profile = pkgs.buildEnv {
+  profile = buildEnv {
     inherit name;
     extraOutputsToInstall = [ "out" "bin" "lib" ];
     inherit paths;
